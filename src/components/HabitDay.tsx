e0319fec8b81728783962b7d2b0e1d38d7f8ac47
MarianoArthur <marianocapiliku@gmail.com>
2023-01-20 22:52:11 +0100

    Initial commit

---
+++ src/components/HabitDay.tsx
@@ -0,0 +1,11 @@
+// interface HabitProps {
+//   completed: number;
+// }
+
+export function HabitDay() {
+  return (
+    <>
+      <div className=" w-9 h-9 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    </>
+  );
+}
